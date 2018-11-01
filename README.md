dc1920b8c7d7000f0a4facfad2608dccfd320793
Clouie Gonzalez <clouie.tantuan@hotmail.com>
2018-11-01 17:21:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## [My personal website](https://typeclo.com/)
---
+++ index.js
@@ -0,0 +1,5 @@
+function clearForm() {
+    setTimeout(function () {
+        document.getElementById("emailForm").reset();
+    }, 100);
+}
